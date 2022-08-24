51250383b876371f274ec19853c4306063197eb4
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-08-24 15:16:15 +0300

    1 task finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timer = {
+  intervalId: null,
+  isActive: false,
+};
+
+startBtn.addEventListener('click', () => {
+  if (timer.isActive) {
+    startBtn.disabled = false;
+  }
+  startBtn.disabled = true;
+  timer.intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timer.intervalId);
+  startBtn.disabled = false;
+});
