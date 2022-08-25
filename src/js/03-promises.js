e55820af7ffce85da7ef963b1c91752d4ed402e6
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-08-25 23:54:38 +0300

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formInput = document.querySelector('.form');
+const createBtn = document.querySelector('button');
+
+const promise = new Promise((resolve, reject) => {});
+
+createBtn.addEventListener('submit', createPromise);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +15,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notify.warning(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
